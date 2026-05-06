7479087f9098c6560f83438e111c8bb2feb484d5
Verly1969 <eddyverly@icloud.com>
2026-05-06 08:10:26 +0200

    intial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,17 @@ import { authGuardGuard } from './core/guards/auth-guard';
 import { Profil } from './shared/accueil/profil/profil';
 
 export const routes: Routes = [
+<<<<<<< HEAD
     { path: '', redirectTo: 'login', pathMatch: 'full' }, 
     { path: 'login', component: Login },
     { path: 'profil', component: Profil, canActivate: [authGuardGuard] },
+=======
+    {
+        path: 'newSkill',
+        title: 'Création d\'un skill',
+
+        loadComponent: () => import("./features/layout/skill/new/new")
+            .then(s => s.New)
+    }
+>>>>>>> 1eb4e32 (intial commit)
 ];
