a01ac635e1135a2100578ab7e5fdcb404ffbb3b8
Kévin Brogniez <kbrogniez8@gmail.com>
2026-05-07 16:35:00 +0200

    feat(US10): add CategoryService

---
+++ src/app/core/services/category.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+  private apiUrl = `${environment.apiUrl}/api/Category`;
+
+  constructor(private http: HttpClient) {}
+
+  getById(id: string): Observable<any> {
+    return this.http.get(`${this.apiUrl}/${id}`);
+  }
+
+  update(id: string, data: { name: string; description?: string }): Observable<any> {
+    return this.http.patch(`${this.apiUrl}/${id}`, data);
+  }
+}
