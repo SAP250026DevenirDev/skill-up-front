fb49046cc378982742fd970617e6b782392a976f
Kévin Brogniez <kbrogniez8@gmail.com>
2026-05-07 16:35:53 +0200

    feat(US10): connect update-category component to API

--- src/app/features/admin/categories/update-category/update-category.ts
+++ src/app/features/admin/categories/update-category/update-category.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { CategoryService } from '../../../../core/services/category.service';
 
 @Component({
   selector: 'app-update-category',
@@ -10,11 +11,13 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class UpdateCategory implements OnInit {
   updateForm: FormGroup;
+  categoryId!: string;
 
   constructor(
     private fb: FormBuilder,
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private categoryService: CategoryService
   ) {
     this.updateForm = this.fb.group({
       name: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(100)]],
@@ -23,13 +26,20 @@ export class UpdateCategory implements OnInit {
   }
 
   ngOnInit(): void {
-    // sera connecté à l'API
+    this.categoryId = this.route.snapshot.paramMap.get('id')!;
+    this.categoryService.getById(this.categoryId).subscribe(category => {
+      this.updateForm.patchValue({
+        name: category.name,
+        description: category.description
+      });
+    });
   }
 
   onSubmit(): void {
-    if (this.updateForm.valid) {
-      console.log(this.updateForm.value);
-      // sera connecté à l'API
-    }
+   if (this.updateForm.valid) {
+    this.categoryService.update(this.categoryId, this.updateForm.value).subscribe(() => {
+      this.router.navigate(['/skills-manager']);
+    });
+    } 
   }
 }
