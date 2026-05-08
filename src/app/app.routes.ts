9738baccf5a4ac4b72ef72ea99741d9ec9921467
Verly1969 <eddyverly@icloud.com>
2026-05-08 11:16:07 +0200

    fix: conflits 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,14 +4,9 @@ import { authGuardGuard } from './core/guards/auth-guard';
 import { Profil } from './shared/accueil/profil/profil';
 
 export const routes: Routes = [
-<<<<<<< HEAD
-<<<<<<< HEAD
     { path: '', redirectTo: 'login', pathMatch: 'full' }, 
     { path: 'login', component: Login },
     { path: 'profil', component: Profil, canActivate: [authGuardGuard] },
-=======
-=======
->>>>>>> 1eb4e326692a50823c4b1032bd34965e6233bf66
     {
         path: 'newSkill',
         title: 'Création d\'un skill',
@@ -19,8 +14,4 @@ export const routes: Routes = [
         loadComponent: () => import("./features/layout/skill/new/new")
             .then(s => s.New)
     }
-<<<<<<< HEAD
->>>>>>> 1eb4e32 (intial commit)
-=======
->>>>>>> 1eb4e326692a50823c4b1032bd34965e6233bf66
 ];
